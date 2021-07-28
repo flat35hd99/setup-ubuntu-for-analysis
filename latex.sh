010ebefe647c4c6bebd70cb6eb43f0f4ee1c36bb
Wataru Sugiura <38929316+flat35hd99@users.noreply.github.com>
2021-07-28 13:19:50 +0900

    Create latex.sh

---
+++ latex.sh
@@ -0,0 +1,11 @@
+apt install texlive-latex-recommended\
+            texlive-latex-extra\
+            dvipng
+
+# install type1cm package
+wget http://mirrors.ctan.org/macros/latex/contrib/type1cm.zip 
+unzip type1cm.zip -d /tmp/type1cm 
+cd /tmp/type1cm/type1cm/ && sudo latex type1cm.ins
+sudo mkdir /usr/share/texmf/tex/latex/type1cm 
+sudo cp /tmp/type1cm/type1cm/type1cm.sty /usr/share/texmf/tex/latex/type1cm 
+sudo texhash
